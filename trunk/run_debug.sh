17e52f26659a6249056c4f5d5059b7e9b4c7e1a9
Chifeng Wen <daviesx66@gmail.com>
2015-10-18 08:07:58 +0000

    git-svn-id: https://svn.code.sf.net/p/x3drenderlib/code@72 06244750-ab4d-4373-8b0e-77f16ea15ae9

--- trunk/run_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PATH=/home/davis/x3drenderlib-code/trunk/netbeans/usr/:$PATH
-PATH=/home/davis/x3drenderlib-code/trunk/netbeans/x3dcore/:$PATH
-
-export PATH
-
-/home/davis/x3drenderlib-code/trunk/netbeans/sampleentry/dist/Debug/GNU-Linux-x86/sampleentry
